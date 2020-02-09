3443952446f33366f7c848dc17d551c1e52bbf45
reza ghanbari <rezaqanbari1925@gmail.com>
2020-02-09 20:02:19 +0330

    addRestaurant added to git.

--- src/main/java/org/kharchal/co/Main.java
+++ src/main/java/org/kharchal/co/Main.java
@@ -1,6 +1,7 @@
 package org.kharchal.co;
 
 import exceptions.InvalidInputInstructionException;
+import structures.Restaurant;
 import systemHandlers.SystemManager;
 
 import java.util.Scanner;
@@ -11,6 +12,11 @@ public class Main {
 
     public static void main( String[] args ) {
 
+    }
+
+    private static void addRestaurant(String jsonData) throws Exception{
+        Restaurant restaurant = Restaurant.deserializeFromJson(jsonData);
+        _systemManager.addRestaurant(restaurant);
     }
 
 }
