667624637ec573a16b8ea4c12a3a1dba8b4e2013
reza ghanbari <rezaqanbari1925@gmail.com>
2020-02-09 13:17:16 +0330

    systemHandler singleton instantiation added

--- src/main/java/systemHandlers/SystemManager.java
+++ src/main/java/systemHandlers/SystemManager.java
@@ -1,4 +1,28 @@
 package systemHandlers;
 
+import exceptions.FoodIsRegisteredException;
+import exceptions.RestaurantDoesntExistException;
+import exceptions.RestaurantIsRegisteredException;
+import structures.Food;
+import structures.Restaurant;
+
+import java.util.ArrayList;
+
 public class SystemManager {
+
+
+    private static SystemManager _instance;
+    private DataHandler _dataHandler;
+
+    private SystemManager() {
+        _dataHandler = DataHandler.getInstance();
+    }
+
+    public static SystemManager getInstance() {
+        if (_instance == null) {
+            _instance = new SystemManager();
+        }
+        return _instance;
+    }
+
 }
