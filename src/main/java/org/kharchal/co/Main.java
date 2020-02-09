3040572deef822f6882363ac84b1af665094316c
reza ghanbari <rezaqanbari1925@gmail.com>
2020-02-09 20:00:59 +0330

    Main function added

--- src/main/java/org/kharchal/co/Main.java
+++ src/main/java/org/kharchal/co/Main.java
@@ -1,4 +1,16 @@
 package org.kharchal.co;
 
+import exceptions.InvalidInputInstructionException;
+import systemHandlers.SystemManager;
+
+import java.util.Scanner;
+
 public class Main {
+
+    static SystemManager _systemManager = SystemManager.getInstance();
+
+    public static void main( String[] args ) {
+
+    }
+
 }
