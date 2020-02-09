f43065b210a275c517d232a38e9422bfa9d4b9fb
alireza salemi <alirezasalemi7@gmail.com>
2020-02-09 03:04:21 -0500

    getters added

--- src/main/java/systemHandlers/DataHandler.java
+++ src/main/java/systemHandlers/DataHandler.java
@@ -1,4 +1,29 @@
 package systemHandlers;
 
+import structures.Location;
+import structures.Restaurant;
+import structures.User;
+
+import java.util.HashMap;
+
 public class DataHandler {
+
+    private User _user = new User(new Location(0,0));
+    private HashMap<String, Restaurant> _restaurants;
+    private DataHandler _instance;
+
+    private DataHandler(){}
+
+    public DataHandler getInstance(){
+        if(_instance==null){
+            _instance = new DataHandler();
+        }
+        return _instance;
+    }
+
+    public User getUser(){return _user;}
+
+    public HashMap<String, Restaurant> getAllRestaurant(){ return _restaurants;}
+
+
 }
