83e69d3f46f75bc4e8827103298e94c13bec36c7
alireza salemi <alirezasalemi7@gmail.com>
2020-02-12 08:38:25 -0500

    server fetch data from external server functionality added

--- src/main/java/org/kharchal/co/Main.java
+++ src/main/java/org/kharchal/co/Main.java
@@ -9,6 +9,7 @@ import exceptions.RestaurantDoesntExistException;
 import structures.Food;
 import structures.Restaurant;
 import systemHandlers.SystemManager;
+import web.server.LoghmehServer;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -19,91 +20,7 @@ public class Main {
     static SystemManager _systemManager = SystemManager.getInstance();
 
     public static void main( String[] args ) {
-        Scanner scanner = new Scanner(System.in);
-        do {
-            String instruction = scanner.nextLine();
-            parseInstruction(instruction.trim());
-        } while ((scanner.hasNext()));
+        LoghmehServer server = new LoghmehServer();
+        server.startServer();
     }
-
-    private static void addRestaurant(String jsonData) throws Exception{
-        Restaurant restaurant = Restaurant.deserializeFromJson(jsonData);
-        _systemManager.addRestaurant(restaurant);
-    }
-
-    private static void addFood(String jsonData) throws Exception{
-        Food food = Food.deserializeFromJson(jsonData);
-        _systemManager.addFood(food);
-    }
-
-    private static void getRestaurants(){
-        ArrayList<String> names = _systemManager.getAllRestaurants();
-        for (String name : names){
-            System.out.println(name);
-        }
-    }
-
-    private static void getRestaurant(String jsonData) throws IOException, RestaurantDoesntExistException, InvalidToJsonException {
-        JsonNode node = (new ObjectMapper()).readTree(jsonData);
-        String name = node.get("id").asText().trim();
-        Restaurant restaurant = _systemManager.getRestaurantById(name);
-        System.out.println(restaurant.toJson());
-    }
-
-    private static void getFood(String jsonData) throws IOException, InvalidToJsonException, RestaurantDoesntExistException, FoodDoesntExistException {
-        JsonNode node = (new ObjectMapper()).readTree(jsonData);
-        String foodName = node.get("foodName").asText().trim();
-        String restaurantName = node.get("restaurantName").asText().trim();
-        Food food = _systemManager.getFood(restaurantName, foodName);
-        System.out.println(food.toJson());
-    }
-
-    private static void getRecommendedRestaurants(){
-        ArrayList<Restaurant> restaurants = _systemManager.getRecommendedRestaurants(_systemManager.getUser());
-        for(Restaurant restaurant : restaurants){
-            System.out.println(restaurant.getName());
-        }
-    }
-
-    private static void parseInstruction(String inst){
-        int indexOfData = inst.indexOf(" ");
-        indexOfData = (indexOfData == -1) ? inst.length() : indexOfData;
-        String instruction = inst.substring(0, indexOfData);
-        try {
-            switch (instruction){
-                case "addRestaurant" : {
-                    addRestaurant(inst.substring(indexOfData).trim());
-                };break;
-                case "addFood" : {
-                    addFood(inst.substring(indexOfData).trim());
-                };break;
-                case "getRestaurants" : {
-                    getRestaurants();
-                };break;
-                case "getRestaurant" : {
-                    getRestaurant(inst.substring(indexOfData).trim());
-                };break;
-                case "getFood" : {
-                    getFood(inst.substring(indexOfData).trim());
-                };break;
-                case "addToCart" : {
-                    _systemManager.addToCart(inst.substring(indexOfData).trim());
-                };break;
-                case "getCart" : {
-                    _systemManager.getCart();
-                };break;
-                case "finalizeOrder" : {
-                    _systemManager.finalizeOrder();
-                };break;
-                case "getRecommendedRestaurants" : {
-                    getRecommendedRestaurants();
-                };break;
-                default: throw new InvalidInputInstructionException();
-            }
-        }
-        catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-    }
-
 }
--- src/main/java/web/server/LoghmehServer.java
+++ src/main/java/web/server/LoghmehServer.java
@@ -1,11 +1,26 @@
 package web.server;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import exceptions.InvalidJsonInputException;
+import exceptions.RestaurantIsRegisteredException;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import io.javalin.http.HandlerType;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.jetbrains.annotations.NotNull;
+import structures.Restaurant;
 import systemHandlers.SystemManager;
+import web.html.HtmlPageMaker;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class LoghmehServer {
 
@@ -13,10 +28,12 @@ public class LoghmehServer {
     private Javalin _server;
     private boolean _started;
     private int _port = 8080;
+    private HtmlPageMaker _pageMaker;
 
     public LoghmehServer(){
         _started = false;
         _system = SystemManager.getInstance();
+        _pageMaker = new HtmlPageMaker();
     }
 
     public LoghmehServer(int port){
@@ -24,10 +41,69 @@ public class LoghmehServer {
         _port = port;
     }
 
+    private String sendGetRequestToGetDataOnStart(){
+        StringBuilder jsonBody = new StringBuilder();
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpGet request = new HttpGet("http://138.197.181.131:8080/restaurants");
+            HttpResponse response = client.execute(request);
+            if(response.getStatusLine().getStatusCode()!=200){
+                System.out.println("cannot get data from external server. terminate.");
+                System.exit(1);
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            String line;
+            while ((line = reader.readLine()) != null){
+                jsonBody.append(line);
+            }
+            return jsonBody.toString();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+            System.exit(1);
+            return null;
+        }
+    }
+
+    private ArrayList<Restaurant> externalServerBodyParser(String jsonBody){
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayList<Restaurant> restaurants = new ArrayList<>();
+        try {
+            JsonNode root = mapper.readTree(jsonBody);
+            if(root.isArray()){
+                for(JsonNode restaurant : root){
+                    String utf8 = new String(restaurant.toString().getBytes(),"UTF-8");
+                    Restaurant encodedObject = Restaurant.deserializeFromJson(utf8);
+                    restaurants.add(encodedObject);
+                }
+            }
+            return restaurants;
+        }
+        catch (IOException | InvalidJsonInputException e){
+            System.err.println("invalid input from external server. terminated.");
+            System.exit(1);
+            return null;
+        }
+    }
+
+    private void fetchFromExternalServer(){
+        ArrayList<Restaurant> restaurants = externalServerBodyParser(sendGetRequestToGetDataOnStart());
+        try {
+            for (Restaurant restaurant : restaurants){
+                _system.addRestaurant(restaurant);
+            }
+        }
+        catch (RestaurantIsRegisteredException e){
+            System.err.println("duplicated restaurant from external server. terminate.");
+            System.exit(1);
+        }
+    }
+
     public void startServer(){
         if(_started){
             return;
         }
+        fetchFromExternalServer();
         _server = Javalin.create().start(_port);
         setRoutes();
     }
