f80844f0ae3fc4c09e446a81a94f27741697b7b5
r.ghanbari <rezaqanbari1925@gmail.com>
2020-02-13 19:58:15 +0330

    Add makeRestaurantPage to HTMLPageMaker

--- src/main/java/web/html/HtmlPageMaker.java
+++ src/main/java/web/html/HtmlPageMaker.java
@@ -1,5 +1,6 @@
 package web.html;
 
+import models.Food;
 import models.Restaurant;
 import models.User;
 
@@ -16,11 +17,10 @@ public class HtmlPageMaker {
             pageContent = new String(Files.readAllBytes(Paths.get("src/main/resources/allRestaurantsPage/allRestaurants.txt")));
             tableRowContent = new String(Files.readAllBytes(Paths.get("src/main/resources/allRestaurantsPage/tableRow.txt")));
             for (int i = 0; i < restaurants.size(); i++) {
-                pageContent = pageContent.replace("TableRows"
-                                , tableRowContent.replace("RestaurantId", restaurants.get(i).getId())
-                                    .replace("ImageSource", restaurants.get(i).getLogoAddress())
-                                        .replace("RestaurantName", restaurants.get(i).getName())
-                                            .replace("RestaurantAddress", "/restaurants/" + restaurants.get(i).getId()) + ((i < (restaurants.size() - 1)) ? "TableRows" : ""));
+                pageContent = pageContent.replace("TableRows", tableRowContent.replace("RestaurantId", restaurants.get(i).getId())
+                                .replace("ImageSource", restaurants.get(i).getLogoAddress())
+                                    .replace("RestaurantName", restaurants.get(i).getName())
+                                        .replace("RestaurantAddress", "/restaurants/" + restaurants.get(i).getId()) + ((i < (restaurants.size() - 1)) ? "TableRows" : ""));
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -28,8 +28,32 @@ public class HtmlPageMaker {
         return pageContent;
     }
 
+    private String makeMenuContents(String pageContent, ArrayList<Food> foods) {
+        String foodInfoContent = null;
+        try {
+            foodInfoContent = new String(Files.readAllBytes(Paths.get("src/main/resources/RestaurantPage/foodInfo.txt")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < foods.size(); i++) {
+            pageContent = pageContent.replace("FoodInfo", foodInfoContent.replace("FoodImage", foods.get(i).getImageAddress())
+                    .replace("FoodName", foods.get(i).getName()).replace("FoodPrice", foods.get(i).getPrice() + "")
+                    + (((i < (foods.size() - 1)) ? "FoodInfo" : "")));
+        }
+        return pageContent;
+    }
+
     public String makeRestaurantPage(Restaurant restaurant){
-        return null;
+        String pageContent = null;
+        try {
+            pageContent = new String(Files.readAllBytes(Paths.get("src/main/resources/RestaurantPage/restaurantInfo.txt")));
+            pageContent = pageContent.replace("RestauratnId", restaurant.getId()).replace("RestaurantName", restaurant.getName())
+                            .replace("xLocation", restaurant.getLocation().getX() + "").replace("yLocation", restaurant.getLocation().getY() + "")
+                                .replace("RestaurantImage", restaurant.getLogoAddress());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return makeMenuContents(pageContent, restaurant.getMenu());
     }
 
     public String makeProfilePage(User user){
