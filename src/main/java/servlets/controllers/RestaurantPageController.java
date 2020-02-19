05b363767e21ae8c17104826cff4fb713cf03da8
r.ghanbari <rezaqanbari1925@gmail.com>
2020-02-20 02:35:48 +0330

    Add RestaurantPageController to controllers

--- src/main/java/servlets/controllers/RestaurantPageController.java
+++ src/main/java/servlets/controllers/RestaurantPageController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/restaurants/*")
 public class RestaurantPageController extends HttpServlet {
@@ -18,17 +19,22 @@ public class RestaurantPageController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = req.getPathInfo().substring(1);
+        RequestDispatcher dispatcher;
         try {
             Restaurant restaurant = SystemManager.getInstance().getRestaurantById(id);
             if (restaurant.getLocation().getDistance(SystemManager.getInstance().getUser().getLocation()) <= 170) {
                 req.setAttribute("restaurant", restaurant);
-                RequestDispatcher dispatcher = req.getRequestDispatcher("/pages/restaurants/restaurantInfo.jsp");
-                dispatcher.forward(req, resp);
+                dispatcher = req.getRequestDispatcher("/pages/restaurants/restaurantInfo.jsp");
             } else {
-//                403 forbidden
+                req.setAttribute("errorCode", "403");
+                req.setAttribute("message", "You're not allowed to see this page.");
+                dispatcher = req.getRequestDispatcher("/pages/errors/errorPage.jsp");
             }
         } catch (RestaurantDoesntExistException e) {
-//          404 not found
+            req.setAttribute("errorCode", "404");
+            req.setAttribute("message", "Page not found.");
+            dispatcher = req.getRequestDispatcher("/pages/errors/errorPage.jsp");
         }
+        dispatcher.forward(req, resp);
     }
 }
