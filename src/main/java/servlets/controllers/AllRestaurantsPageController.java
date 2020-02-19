752f46fd14a31468532caa25d8e529db065ad492
r.ghanbari <rezaqanbari1925@gmail.com>
2020-02-19 22:26:28 +0330

    Add allRestaurantController to controllers

--- src/main/java/servlets/controllers/AllRestaurantsPageController.java
+++ src/main/java/servlets/controllers/AllRestaurantsPageController.java
@@ -1,4 +1,25 @@
 package servlets.controllers;
 
-public class AllRestaurantsPageController {
+import models.Restaurant;
+import systemHandlers.SystemManager;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/restaurants")
+public class AllRestaurantsPageController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ArrayList<Restaurant> restaurants = SystemManager.getInstance().getAllRestaurantsInfo();
+        req.setAttribute("restaurants", restaurants);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/pages/restaurants/allRestaurants.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
