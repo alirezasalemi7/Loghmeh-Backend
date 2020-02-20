8aaf15309e99efa39db5a9657ae603c903099729
r.ghanbari <rezaqanbari1925@gmail.com>
2020-02-20 03:44:37 +0330

    Add profilePageController to controllers

--- src/main/java/servlets/controllers/ProfilePageController.java
+++ src/main/java/servlets/controllers/ProfilePageController.java
@@ -1,4 +1,28 @@
 package servlets.controllers;
 
-public class ProfilePageController {
+import models.OrderItem;
+import models.User;
+import systemHandlers.SystemManager;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/profile")
+public class ProfilePageController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = SystemManager.getInstance().getUser();
+        ArrayList<OrderItem> orders = user.getCart().getOrders();
+        req.setAttribute("user", user);
+        req.setAttribute("orders", orders);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/pages/profile/Profile.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
