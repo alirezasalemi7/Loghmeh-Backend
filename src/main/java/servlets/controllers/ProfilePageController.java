2cfde2191f1ba52f9e3c9688ddf70c8ea63d0423
r.ghanbari <rezaqanbari1925@gmail.com>
2020-02-20 19:04:14 +0330

    Send flags to Profile.jsp from profilePageController

--- src/main/java/servlets/controllers/ProfilePageController.java
+++ src/main/java/servlets/controllers/ProfilePageController.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -20,8 +21,16 @@ public class ProfilePageController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User user = SystemManager.getInstance().getUser();
         ArrayList<OrderItem> orders = user.getCart().getOrders();
+        System.err.println("SIZE" + orders.size());
         req.setAttribute("user", user);
         req.setAttribute("orders", orders);
+        HttpSession session = req.getSession();
+        Object creditIsNegative = session.getAttribute("creditIsNegative");
+        session.removeAttribute("creditIsNegative");
+        Object successFullAddCredit = session.getAttribute("successFullAddCredit");
+        session.removeAttribute("successFullAddCredit");
+        session.setAttribute("creditIsNegative", (creditIsNegative == null) || ((boolean) creditIsNegative));
+        session.setAttribute("successFullAddCredit", (creditIsNegative == null) || ((boolean) successFullAddCredit));
         RequestDispatcher dispatcher = req.getRequestDispatcher("/pages/profile/Profile.jsp");
         dispatcher.forward(req, resp);
     }
