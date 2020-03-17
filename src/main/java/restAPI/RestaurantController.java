ed14155b35f7f24c2268051363209c9e00b55008
alireza salemi <alirezasalemi7@gmail.com>
2020-03-17 23:52:09 +0330

    Add getFood api

--- src/main/java/restAPI/RestaurantController.java
+++ src/main/java/restAPI/RestaurantController.java
@@ -6,8 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import exceptions.FoodDoesntExistException;
 import exceptions.InvalidToJsonException;
 import exceptions.RestaurantDoesntExistException;
+import models.Food;
+import models.NormalFood;
 import models.Restaurant;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -69,5 +72,43 @@ public class RestaurantController {
             return new ResponseEntity<>(answerJson,HttpStatus.INTERNAL_SERVER_ERROR);
         }
    }
+
+    @RequestMapping(value = "/{id}/{fid}",method = RequestMethod.GET)
+    public ResponseEntity<Object> getNormalFood(
+            @PathVariable(value = "id",required = true) String restaurantId,
+            @PathVariable(value = "id",required = true) String foodId
+    ){
+        try {
+            Restaurant restaurant = SystemManager.getInstance().getRestaurantById(restaurantId);
+            if (restaurant.getLocation().getDistance(SystemManager.getInstance().getUser().getLocation()) <= 170) {
+                NormalFood food = restaurant.getNormalFoodByName(foodId);
+                JsonNode answerJson = mapper.readTree(NormalFood.toJson());
+                return new ResponseEntity<>(answerJson, HttpStatus.OK);
+            } else {
+                ObjectNode answerJson = factory.objectNode();
+                answerJson.put("status", 403);
+                answerJson.put("description", "restaurant not in range");
+                return new ResponseEntity<>(answerJson, HttpStatus.FORBIDDEN);
+            }
+        } catch (RestaurantDoesntExistException e) {
+            ObjectNode answerJson = factory.objectNode();
+            answerJson.put("status", 404);
+            answerJson.put("description", "restaurant does not exist");
+            return new ResponseEntity<>(answerJson, HttpStatus.NOT_FOUND);
+        }
+        catch (FoodDoesntExistException e){
+            ObjectNode answerJson = factory.objectNode();
+            answerJson.put("status", 404);
+            answerJson.put("description", "food does not exist");
+            return new ResponseEntity<>(answerJson, HttpStatus.NOT_FOUND);
+        }
+        catch (InvalidToJsonException| IOException e){
+            ObjectNode answerJson = factory.objectNode();
+            answerJson.put("status", 500);
+            answerJson.put("description", "internal server error");
+            return new ResponseEntity<>(answerJson,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 }
